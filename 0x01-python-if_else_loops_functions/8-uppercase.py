5ccb315fd17c9637f924dbf90f0b19394dc5e6cd
pere isenah <pereisenah@gmail.com>
2023-01-04 22:00:43 +0100

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     for x in str:
         x = ord(x)
         if x in range(ord("a"), ord("z")+1):
             x = x-32 
-        print(chr(x) , end = "")
-    print("\n")
+        print("{}".format(chr(x)), end = "")
+    print()
