4fd56dc0f981107d56063527e45ac6e695109990
Pere-Isenah <pereisenah@gmail.com>
2023-04-11 19:14:57 +0100

    fix task 1-6

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 2) {
   console.log('No argument');
 } else if (process.argv[2]) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
   console.log('Argument found');
-} 
+}
