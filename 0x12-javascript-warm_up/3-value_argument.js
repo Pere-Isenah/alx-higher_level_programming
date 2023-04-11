5cb2d9661678f48f6c61c9ad4c3985986d1888db
Pere-Isenah <pereisenah@gmail.com>
2023-04-11 18:57:42 +0100

    fix task 1-6

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
-let count = 0;
-for(let element in process.argv){
-    count++;
-}
-if(count <= 2){
-    console.log("No argument");
-}
-else{
-    console.log(process.argv[2]);
+if (!process.argv[2]) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
