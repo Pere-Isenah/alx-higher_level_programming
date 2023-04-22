e11db8a9cb9ed8a138da77e0077462b46c3e7e83
Pere-Isenah <pereisenah@gmail.com>
2023-04-22 11:08:13 +0100

    task6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,9 +15,10 @@ class State(Base):
     A SQLAlchemy ORM model representing a state.
 
     Attributes:
+        __tablename__(str): the name of the MySql table to store states. 
         id (int): The unique identifier for the state.
         name (str): The name of the state.
     """
     __tablename__ = "states"
-    id = Column(Integer,nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(length= 128), nullable=False)
