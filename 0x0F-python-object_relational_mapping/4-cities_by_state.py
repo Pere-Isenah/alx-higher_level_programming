5f6e2356beeec98520ce5e4a4dec4bc2a765f659
Pere-Isenah <pereisenah@gmail.com>
2023-04-22 07:04:05 +0100

    fix task 1-5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,15 +7,19 @@ Write a script that lists all cities from the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user= sys.argv[1], passwd= sys.argv[2], db= sys.argv[3])
-cur = db.cursor()
-
-query="SELECT c.id, c.name, s.name FROM cities as c\
-                    inner join states as s\
-                    ON c.state_id = s.id"
-cur.execute(query)
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+if __name__ == "__main__":
+    """
+     Connect to the MySQL database using the provided credentials
+    """
+    db = MySQLdb.connect(user= sys.argv[1], passwd= sys.argv[2], db= sys.argv[3])
+    cur = db.cursor()
+    
+    query="SELECT c.id, c.name, s.name FROM cities as c\
+                        inner join states as s\
+                        ON c.state_id = s.id"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
